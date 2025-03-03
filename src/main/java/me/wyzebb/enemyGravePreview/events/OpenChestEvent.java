16781d01ae7158001dbbdf683566f3993a5c8dc6
Wyzebb <BenSJSmith8@gmail.com>
2025-03-03 12:01:40 +0000

    Use interact event instead of angelchest event and add null checks

--- src/main/java/me/wyzebb/enemyGravePreview/events/OpenChestEvent.java
+++ src/main/java/me/wyzebb/enemyGravePreview/events/OpenChestEvent.java
@@ -1,34 +1,51 @@
 package me.wyzebb.enemyGravePreview.events;
 
 import de.jeff_media.angelchest.AngelChest;
-import de.jeff_media.angelchest.events.AngelChestOpenEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+
+import static me.wyzebb.enemyGravePreview.EnemyGravePreview.plugin;
 
 public class OpenChestEvent implements Listener {
     private String invName = "Player's Grave";
 
     @EventHandler
-    public void onOpenChest(AngelChestOpenEvent event) {
-        AngelChest chest = event.getAngelChest();
+    public void onOpenChest(PlayerInteractEvent event) {
+        Player chestOpener = event.getPlayer();
+
+        if (event.getClickedBlock() == null) {
+            return;
+        }
+
+        if (event.getClickedBlock().getType() != Material.CHEST) {
+            return;
+        }
+
+        AngelChest chest = plugin.angelChestPlugin.getAngelChestAtBlock(event.getClickedBlock());
+
+        if (chest == null) {
+            return;
+        }
+
         OfflinePlayer chestOwner = chest.getPlayer();
-        OfflinePlayer chestOpener = event.getPlayer();
 
         if (chest.isProtected()) {
-            if (chestOwner != chestOpener) {
-                // ENEMY
+            if (!Objects.equals(chestOwner.getName(), chestOpener.getName())) {
+                // The player is not the chest owner
                 invName = chestOwner.getName() + "'s Grave";
-                Player player = (Player) chestOpener;
 
                 List<ItemStack> itemStacks = new ArrayList<>();
 
@@ -36,13 +53,15 @@ public class OpenChestEvent implements Listener {
                 itemStacks.add(chest.getOffhandItem());
                 itemStacks.addAll(Arrays.asList(chest.getStorageInv()));
 
-                Inventory inv = Bukkit.createInventory(player, (9 * 3) + 5, invName);
+                Inventory inv = Bukkit.createInventory(chestOpener, 9 * 4, invName);
 
                 for (ItemStack itemStack: itemStacks) {
-                    inv.addItem(itemStack);
+                    if (itemStack != null) {
+                        inv.addItem(itemStack);
+                    }
                 }
 
-                player.openInventory(inv);
+                chestOpener.openInventory(inv);
             }
         }
     }
