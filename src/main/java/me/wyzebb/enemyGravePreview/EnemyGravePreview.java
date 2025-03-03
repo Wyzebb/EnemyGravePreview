9b47267e0a6a984afd7b745a7ccc450945863f0e
Wyzebb <BenSJSmith8@gmail.com>
2025-03-03 11:58:43 +0000

    Add static reference to plugin

--- src/main/java/me/wyzebb/enemyGravePreview/EnemyGravePreview.java
+++ src/main/java/me/wyzebb/enemyGravePreview/EnemyGravePreview.java
@@ -5,10 +5,12 @@ import me.wyzebb.enemyGravePreview.events.OpenChestEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EnemyGravePreview extends JavaPlugin {
+    public static EnemyGravePreview plugin;
     public AngelChestPlugin angelChestPlugin = (AngelChestPlugin) getServer().getPluginManager().getPlugin("AngelChest");
 
     @Override
     public void onEnable() {
+        plugin = this;
         getServer().getPluginManager().registerEvents(new OpenChestEvent(), this);
     }
 }
